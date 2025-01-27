610d918ae5c0d71570388da8798b8574792f3853
Renan <qwerty142999@gmail.com>
2025-01-27 16:35:11 -0300

    adding-webamp

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 window.onload = function test(){
 
 
@@ -17,6 +19,34 @@ window.onload = function test(){
             }, 1000);
         }
     });
+
+    const Webamp = window.Webamp;
+    if(!Webamp.browserIsSupported()) {
+        alert("Oh no! Webamp does not work!")
+        throw new Error("What's the point of anything?")
+    }
+    
+    
+    // All configuration options are optional.
+    const webamp = new Webamp({
+      // Optional.
+      initialTracks: [{
+        metaData: {
+          artist: "Final Fantasy IX",
+          title: "Fairy Battle",
+        },
+        // NOTE: Your audio file must be served from the same domain as your HTML
+        // file, or served with permissive CORS HTTP headers:
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+        // Can be downloaded from: https://github.com/captbaritone/webamp/raw/master/mp3/llama-2.91.mp3
+        url: "sounds/fairy-battle.mp3"
+      }],
+      initialSkin: {
+        url: "img/DJ_LAIN_Serial_ExP_2.wsz"
+      },
+    });
+    
+    webamp.renderWhenReady(document.getElementById('winamp-container'));
 
 
 }
